f8924a5f1c3354a1a93441710de1b5aa5a85f147
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2022-06-11 16:48:57 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,28 +1,48 @@
+
+
 new Swiper('.swiper', {
     mousewheel: {
       },
       direction: 'vertical',
       speed: 800,
-      slidesPerView: 'auto', 
+    //   slidesPerView: 1, 
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+});
+
+new Swiper('.sostav__slider', {
+    mousewheel: {
+      },
+      direction: 'vertical',
+    slidesPerView: 8, 
+    spaceBetween: 10,
+    simulateTouch: false,
 });
 
 new Swiper('.slider', {
     mousewheel: {
       },
-      freeMode: true,
       nested: true,
       direction: 'vertical',
       speed: 800,
+      freeMode: {
+        enabled: true,
+      },
 });
 
-new Swiper('.sliderForm', {
+new Swiper('.swiper-form', {
     mousewheel: {
+    },
+    nested: true,
+    direction: 'vertical',
+    freeMode: {
+        enabled: true,
       },
-      freeMode: true,
-      nested: true,
-      direction: 'vertical',
-      height: null,
-});
+    autoHeight: true,
+})
 
 document.body.onload = function (){
     setTimeout(function(){
@@ -34,14 +54,81 @@ document.body.onload = function (){
     
 }
 
-let slide = document.querySelector('.form-slide');
+// let slide = document.querySelector('.form-slide');
 
-document.addEventListener('mousemove', function(){
-    slide.style.height = "auto";
-})
+// document.addEventListener('mousemove', function(){
+//     slide.style.height = "auto";
+// })
 
 // let last = document.querySelector('.last');
 
 // document.addEventListener('mousemove', function(){
 //     last.style.height = '.5px';
 // })
+
+let btn = document.querySelectorAll('.btn');
+
+btn.forEach(element => {
+    element.addEventListener('click', function(){
+        document.querySelector('.lsat').classList.toggle('active');
+        document.querySelector('.qweqwe').classList.toggle('active');
+    })
+});
+
+//дополнительная информаця/состав
+
+document.querySelector('.more').addEventListener('click', function(){
+    document.querySelector('.tovar-podrobnee').classList.add('active');
+    document.querySelector('.popap__close').classList.add('active')
+})
+
+document.querySelector('.popap__close').addEventListener('click', function(){
+    document.querySelector('.tovar-podrobnee').classList.remove('active');
+    document.querySelector('.popap__close').classList.remove('active')
+    document.querySelector('.sostav').classList.remove('active')
+})
+
+document.querySelector('.sostavbtn').addEventListener('click', function(){
+    document.querySelector('.sostav').classList.add('active');
+    document.querySelector('.popap__close').classList.add('active')
+})
+
+let slides = document.querySelectorAll('.slide__sostav');
+
+document.addEventListener('wheel', function(){
+    for (i = 0; i < slides.length; ++i) {
+        if(slides[i].classList.contains('swiper-slide-active')){
+            slides[i+7].classList.add('sostavPrev');
+        } else if(!slides[i].classList.contains('swiper-slide-active')){
+            slides[i+7].classList.remove('sostavPrev');
+        }
+    }
+})
+document.addEventListener('mousemove', function(){
+    for (i = 0; i < slides.length; ++i) {
+        if(slides[i].classList.contains('swiper-slide-active')){
+            slides[i+7].classList.add('sostavPrev');
+        } else if(!slides[i].classList.contains('swiper-slide-active')){
+            slides[i+7].classList.remove('sostavPrev');
+        }
+    }
+})
+
+slides.forEach(element => {
+    element.addEventListener('click', function(){
+        let textId = element.getAttribute('data-t');
+        let text = document.querySelector(textId);
+        let textdel = document.querySelectorAll('.sostavtext');
+        textdel.forEach(element => {
+            element.classList.remove('active')
+        });
+        text.classList.add('active')
+
+    })
+});
+
+//форма
+
+document.querySelector('.formBtn').addEventListener('click', function(){
+    document.querySelector('.forma').classList.add('active');
+})
